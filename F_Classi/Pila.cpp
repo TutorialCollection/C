e681a204643becbe644487ee31bb20365c7943d1
Sandro Gallo <5992750+sandrogallo@users.noreply.github.com>
2024-03-11 08:45:08 +0000

    Costruzione della classe Pila

---
+++ F_Classi/Pila.cpp
@@ -0,0 +1,69 @@
+/*
+** Pila.cpp
+** Classe Pila di interi - Sandro Gallo - 11/03/2024
+*/
+
+#include <iostream>
+using namespace std;
+
+
+class Pila {
+
+    private:
+
+    int size, len, delta;
+    int * v;
+
+    public:
+
+    Pila( int size, int delta ) {
+        this->size = size;
+        this->delta = delta;
+        len = 0;
+        v = new int[size];
+    }
+
+    void push( int n ) {
+        if (len==size) {
+            // cout << "Estendo da " << size << " a " << size+delta << endl; // ******** DEBUG 
+            int * nuovov = new int[size + delta];
+            for (int i=0; i<size; i++) nuovov[i] = v[i];
+            size += delta;
+            delete[] v;
+            v = nuovov;
+        }
+        v[len] = n;
+        len++; 
+    }
+
+    int pop() {
+        if (len==0) { cout << "\nERRORE Pila Vuota\n"; return 0;}
+        return v[--len];
+    }
+
+    int getElement( int index ) {
+        return v[index];
+    }
+
+    void setElement( int index, int newvalue ) {
+        v[index] = newvalue;
+    }
+
+    void print() {
+        cout << "Contenuto della pila: ";
+        for (int i=0; i<len; i++) cout << v[i] << " ";
+        cout << endl;  
+    }
+
+};
+
+int main(int argc, char * argv[]) {
+
+    Pila vett(10, 10);
+    for (int i=0; i<100; i++) vett.push(i);
+    for (int i=0; i<101; i++) cout << vett.pop() << " ";
+    cout << endl;
+
+    // vett.print();
+
+}
