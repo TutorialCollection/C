03c5c4abd32ce46bd8280c1d651ca22bb24d861f
Sandro Gallo <5992750+sandrogallo@users.noreply.github.com>
2024-05-27 18:33:50 +0000

    2705

--- O_Olimpiadi/menu_giapponese.cpp
+++ O_Olimpiadi/menu_giapponese.cpp
@@ -8,55 +8,50 @@ using namespace std;
 
 // dichiaro variabili e vettori del problema a livello globale, per semplicità
 int B, N;
-vector<int> P(1);
+vector<int> P;
 
-
-
+// funzione che svolge il calcolo spefico del problema
 vector<int> allyoucaneat( ) {
-    int last_remain = B;
-    for (int i=0; i<P.size() i++) {
+    int min_money_remain = B;
+    vector<int> BEST_RESULT;
+    for (int i=0; i<P.size(); i++) {
         if (P[i]>B) continue;
-        int remain = B;
-        vector<int> current;
-        current.push_back(P[i]);
-        remain -= P[i];
+        vector<int> current_plates;
+        int current_money_remain = B - P[i];
+        current_plates.push_back(P[i]);
         for (int j=i+1; j<P.size(); j++) {
-            if (P[j]>remain) continue;
-            current.push_back(P[j]);
-            remain -= P[j];
+            if (P[j]>current_money_remain) continue;
+            current_plates.push_back(P[j]);
+            current_money_remain -= P[j];
         }
-        if (remain==0) return current;
-        plates.push_back(current);
-
+        if ( current_money_remain < min_money_remain ) {
+            BEST_RESULT = current_plates;
+            min_money_remain = current_money_remain;
+        }
+        if (current_money_remain==0) break;
     }
-
-    for (int i=0; i<P.size(); i++) cout << P[i] << " ";
-    cout << endl;
-    return R;
+    return BEST_RESULT;
 }
 
-
+// main che carica i dati di input, chiama la funzione di elaborazione e salva i dati in output
 int main() {
 
-    // apre il file di input e quello di output
-    ifstream fin("menu_giapponese_input.txt");
-    ofstream fout("menu_giapponese_output.txt");
+    // riassegna lo standard I/O ai file di input output
+    freopen("menu_giapponese_input.txt", "r", stdin);
+    freopen("menu_giapponese_output.txt", "w", stdout);
 
-    // leggo dal file di input i valori delle variabili del problema
-    fin >> N >> B;
+    // legge i valori delle variabili del problema (il numero di piatti ed il budget a disposizione)
+    cin >> N >> B;
     P.resize(N);
+    // carica il vettore dei prezzi degli N piatti
+    for (int i=0; i<N; i++) cin >> P[i];
 
-    // carica dalla altre righe del file di input il vettore dei prezzi
-    for (int i=0; i<N; i++) {
-        fin >> P[i];
-    }
-
+    // chiama la funzione di elaborazione specifica del problema
     vector<int> R = allyoucaneat();
 
-    // salva sul file di output i valori calcolati dalla funzione 
+    // salva i prezzi dei piatti scelti per massimizzare l'utilizzo del badget
     for (int i=0; i<R.size(); i++) {
-        fout << R[i];
+        cout << R[i];
+        if (i>0) cout << endl;
     }
-    fin.close();
-    fout.close();
 }
